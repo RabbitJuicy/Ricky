58b2411786ace6ca17b1319ab86a46fa9daeb61a
rickyriver <ricky9929992@hotmail.com>
2025-05-24 22:43:59 +0800

    delete object

--- be/db.js
+++ be/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('jamming', 'admin', '12345678', {
-    host: '127.0.0.1',
-    port: 3307,
+    host: 'database-1.cb0qwawswsxh.ap-southeast-1.rds.amazonaws.com',
+    port: 3306,
     dialect: 'mysql',
 });
 
--- be/index.js
+++ be/index.js
@@ -12,16 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// http://localhost:3001/api/search?term=abc
 
-/*
-
-    req.query is like : 
-
-    {
-        term: "abc"
-    }
-*/
 
 app.get("/api/search", (req, res)=>{
     // const term = req.query['term']
@@ -39,33 +30,34 @@ app.get("/api/db_sync", (req, res)=>{
 
 // ---------------------- Create ssh tunnel and start server -----------------------
 
-const { createTunnel } = require('tunnel-ssh');
+//const { createTunnel } = require('tunnel-ssh');
 
-const tunnelOptions = { autoClose: true };
-const serverOptions = { port: 3307 }; // local port to forward
-const sshOptions = {
-  host: '43.199.71.100',
-  port: 22,
-  username: 'ubuntu',
-  privateKey: require('fs').readFileSync('/Users/jason/Downloads/2025-05@dg-innotech.com.pem')
-};
-const forwardOptions = {
-  srcAddr: '127.0.0.1',
-  srcPort: 3307,
-  dstAddr: 'database-1.cr2ggicgueb4.ap-east-1.rds.amazonaws.com',
-  dstPort: 3306
-};
-
-createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions)
-  .then(([server, conn]) => {
-    console.log('SSH tunnel established');
-    app.listen(port, () => {
+//const tunnelOptions = { autoClose: true };
+//const serverOptions = { port: 3307 }; // local port to forward
+//const sshOptions = {
+//  host: '43.199.71.100',
+//  port: 22,
+//  username: 'ubuntu',
+//  privateKey: require('fs').readFileSync('/Users/jason/Downloads/2025-05@dg-innotech.com.pem')
+//};
+//const forwardOptions = {
+//  srcAddr: '127.0.0.1',
+ // srcPort: 3307,
+//  dstAddr: 'database-1.cr2ggicgueb4.ap-east-1.rds.amazonaws.com',
+//  dstPort: 3306
+//};
+  app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
-  })
-  .catch(err => {
-    console.error('Tunnel error:', err);
-  });
+
+//createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions)
+//  .then(([server, conn]) => {
+//    console.log('SSH tunnel established');
+  
+//  })
+//  .catch(err => {
+ //   console.error('Tunnel error:', err);
+ // });
 
 
 
