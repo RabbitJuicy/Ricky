037e30b4cab0844fb30ee4c66f14f5a666585364
rickyriver <ricky9929992@hotmail.com>
2025-05-26 00:30:24 +0800

    update

--- be/db.js
+++ be/db.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('jamming', 'admin', '12345678', {
+
+    
     host: 'database-1.cb0qwawswsxh.ap-southeast-1.rds.amazonaws.com',
     port: 3306,
     dialect: 'mysql',
